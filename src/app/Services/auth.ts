7ad60a309d1306caf373b9f5ced6476ff9327d47
Daorse <mmorpgplays5@gmail.com>
2024-10-17 20:04:37 +0200

    Reservation Type route auth modifier

--- src/app/Services/auth.ts
+++ src/app/Services/auth.ts
@@ -37,31 +37,31 @@ export async function signUp(authProps: signUp_Props) {
 
 //Login
 export async function signIn(authProps: signIn_Props) {
-	const url = `${process.env.NEXT_PUBLIC_API_URL}auth/signin`;
+  const url = `${process.env.NEXT_PUBLIC_API_URL}auth/SignIn`;
 
-	const axiosConfig = {
-		headers: {
-			"content-type": "application/json;charset=utf-8",
-			"Access-Control-Allow-Origin": "*",
-			"Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-		},
-	};
-	return axios
-		.post(
-			url,
-			{
-				email: authProps.email,
-				password: authProps.password,
-			},
-			axiosConfig,
-		)
-		.then((res) => {
-			return res;
-		})
-		.catch((e) => {
-			toast.error(e.response.data.message, {
-				position: "top-center",
-			});
-			return e;
-		});
+  const axiosConfig = {
+    headers: {
+      "content-type": "application/json;charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    },
+  };
+  return axios
+    .post(
+      url,
+      {
+        email: authProps.email,
+        password: authProps.password,
+      },
+      axiosConfig
+    )
+    .then((res) => {
+      return res;
+    })
+    .catch((e) => {
+      toast.error(e.response.data.message, {
+        position: "top-center",
+      });
+      return e;
+    });
 }
---
+++ src/app/Utils/reservation_type.ts
@@ -0,0 +1,12 @@
+export type reservationAdd_Props = {
+  name: string
+  tours: number
+  cardId: string
+}
+
+export type reservation_Props = {
+  name: string
+  tours: number
+  reservationDate: string
+  cardId: string
+}
