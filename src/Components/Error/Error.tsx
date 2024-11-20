42ed91c9e46e6cf357035a905e222ecfe9488173
Daorse <mmorpgplays5@gmail.com>
2024-11-20 22:36:03 +0100

    test

--- src/Components/Error/Error.tsx
+++ src/Components/Error/Error.tsx
@@ -1,6 +1,6 @@
 const ErrorMsg = (errorName: { error: string }) => {
   return (
-    <p className="text-[#fd3131] text-[14px] mt-1 text-center p-1">
+    <p className="text-[#FD3131] text-[14px] mt-1 text-center p-1">
       Le champs {errorName.error} est requis
     </p>
   );
