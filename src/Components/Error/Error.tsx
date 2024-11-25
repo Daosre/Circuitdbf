3473fd7e68ce81a46ec316dbf804fc3efa914d28
Daorse <mmorpgplays5@gmail.com>
2024-11-25 09:23:28 +0100

    Modification grammaire"

--- src/Components/Error/Error.tsx
+++ src/Components/Error/Error.tsx
@@ -1,7 +1,7 @@
 const ErrorMsg = (errorName: { error: string }) => {
   return (
     <p className="text-red-600 text-[14px] mt-1 text-center p-1">
-      Le champs {errorName.error} est requis
+      Le champ {errorName.error} est requis
     </p>
   );
 };
